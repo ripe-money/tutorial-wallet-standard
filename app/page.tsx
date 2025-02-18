6fc00e33385115331deedcdf7f6dbc61bbd29805
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-02-18 15:54:38 -0500

    Add adapter for wallets without Wallet Standard support

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 'use client';
+import { useEffect } from 'react';
 
 import { getWallets, StandardConnect } from '@wallet-standard/core';
-import { SOLANA_MAINNET_CHAIN } from '@solana/wallet-standard';
+import { registerWalletAdapter, SOLANA_MAINNET_CHAIN } from '@solana/wallet-standard';
+
+import {
+  CoinbaseWalletAdapter, // Need this to expose the Coinbase wallet
+  SolflareWalletAdapter, // Not necessary for Solflare, but has a side effect of showing Solana on MetaMask
+} from '@solana/wallet-adapter-wallets';
+const adapters = [new SolflareWalletAdapter(), new CoinbaseWalletAdapter()];
 
 export default function Home() {
+  // Register adapters for wallets that don't natively support Wallet Standard
+  useEffect(() => {
+    const destructors = adapters.map((adapter) => registerWalletAdapter(adapter, SOLANA_MAINNET_CHAIN));
+    return () => destructors.forEach((destroy) => destroy());
+  }, []);
+
   const availableWallets = getWallets().get();
   console.log(availableWallets);
 
