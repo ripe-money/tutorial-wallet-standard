ff40715ea630f613ca98d5f438266e47f1794252
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-02-18 15:11:24 -0500

    Install DaisyUI

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+      <h1 className="text-3xl font-bold">
+        Available Wallets:
       </h1>
+      <div className="flex flex-col">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <button key={i} className="btn btn-primary m-2">
+            {i + 1}
+          </button>
+        ))}
+      </div>
     </>
   );
 }
