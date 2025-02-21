f76b0875ff1c3892662a25b5cbdee50e27beea16
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-02-21 12:46:23 -0500

    Get USDC balance

--- app/solana.ts
+++ app/solana.ts
@@ -2,14 +2,25 @@ import { WalletAccount } from "@wallet-standard/core";
 
 import { address } from '@solana/web3.js';
 
-import { createSolanaRpc, Rpc, GetBalanceApi } from '@solana/web3.js';
-const rpc: Rpc<GetBalanceApi> = createSolanaRpc('https://sibylla-ghbj3j-fast-mainnet.helius-rpc.com');
+import { createSolanaRpc, Rpc, GetBalanceApi, GetTokenAccountsByOwnerApi } from '@solana/web3.js';
+const rpc: Rpc<GetBalanceApi & GetTokenAccountsByOwnerApi> =
+  createSolanaRpc('https://sibylla-ghbj3j-fast-mainnet.helius-rpc.com');
 
-export const getSolBalance = async (account: WalletAccount) => {
+export const getBalance = async (account: WalletAccount) => {
   const { value: lamports } =
     await rpc.getBalance(address(account.address), { commitment: 'confirmed' }).send();
-  return new Intl.NumberFormat(undefined, { maximumFractionDigits: 5 }).format(
-      // @ts-expect-error This format string is 100% allowed now.
-      `${lamports}E-9`,
+  const formattedSolValue = new Intl.NumberFormat(undefined, { maximumFractionDigits: 5 }).format(
+    // @ts-expect-error This format string is 100% allowed now.
+    `${lamports}E-9`,
   );
+  console.log('Balance:', formattedSolValue, 'SOL');
+
+  const info = await rpc.getTokenAccountsByOwner(
+    address(account.address),
+    { mint: address('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v') }, // USDC
+    { commitment: 'confirmed', encoding: 'jsonParsed' }
+  ).send();
+  info.value.forEach((account) => {
+    console.log('USDC:', account.account.data.parsed.info.tokenAmount.uiAmount);
+  });
 }
