2aa0a625941c368f979d3f73510c0502748d3c3d
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-02-23 14:41:46 -0500

    Return to using WalletAccount type

--- app/context/SelectedAccountContext.tsx
+++ app/context/SelectedAccountContext.tsx
@@ -3,28 +3,16 @@ import { createContext, useState, type ReactNode } from 'react';
 // https://github.com/wallet-standard/wallet-standard/blob/master/packages/core/base/src/wallet.ts
 import type { WalletAccount } from "@wallet-standard/core";
 
-// WalletAccount has fields: address, publicKey, chains, features, label, icon.
-// It's possible for two different wallets to be configured with the same WalletAccount.
-// We add a walletName field to track which wallet the user has connected the account from.
-type AccountWithWallet = WalletAccount & {
-  walletName: string;
-}
-
 const SelectedAccountContext = createContext<{
-  selectedAccount: AccountWithWallet | undefined;
-  setSelectedAccount: (account: AccountWithWallet) => void;
+  selectedAccount: WalletAccount | undefined;
+  setSelectedAccount: (account: WalletAccount) => void;
 }>({
   selectedAccount: undefined,
   setSelectedAccount: () => {},
 });
 
 const SelectedAccountContextProvider = ({ children }: { children: ReactNode }) => {
-  const [selectedAccount, _setSelectedAccount] = useState<AccountWithWallet | undefined>(undefined);
-
-  const setSelectedAccount = (account: AccountWithWallet) => {
-    console.log('Setting selected account:', account);
-    _setSelectedAccount(account);
-  };
+  const [selectedAccount, setSelectedAccount] = useState<WalletAccount | undefined>(undefined);
 
   return (
     <SelectedAccountContext.Provider value={{ selectedAccount, setSelectedAccount }}>
