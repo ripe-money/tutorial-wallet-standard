c9299d245df21cd00bc1158be4f3e4cb93d49173
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-02-23 18:01:10 -0500

    Expose connectUiWallet() instead of setConnectedAccount() in context

--- app/context/ConnectedAccountContext.tsx
+++ app/context/ConnectedAccountContext.tsx
@@ -2,20 +2,33 @@ import { createContext, useState, type ReactNode } from 'react';
 
 // https://github.com/wallet-standard/wallet-standard/blob/master/packages/core/base/src/wallet.ts
 import type { WalletAccount } from "@wallet-standard/core";
+// https://github.com/wallet-standard/wallet-standard/blob/master/packages/ui/core
+import type { UiWallet } from '@wallet-standard/react';
+
+import { connectUiWallet as _connectUiWallet } from '../lib/wallet-standard';
 
 const ConnectedAccountContext = createContext<{
   connectedAccount: WalletAccount | undefined;
-  setConnectedAccount: (account: WalletAccount) => void;
+  connectUiWallet: (wallet: UiWallet) => Promise<void>;
 }>({
   connectedAccount: undefined,
-  setConnectedAccount: () => {},
+  connectUiWallet: () => Promise.resolve(),
 });
 
 const ConnectedAccountContextProvider = ({ children }: { children: ReactNode }) => {
   const [connectedAccount, setConnectedAccount] = useState<WalletAccount | undefined>(undefined);
 
+  const connectUiWallet = async (wallet: UiWallet) => {
+    console.log('Connecting to wallet:', wallet.name);
+    const accounts = await _connectUiWallet(wallet);
+    if (accounts.length > 0) {
+      console.log('Connected accounts:', accounts);
+      setConnectedAccount(accounts[0]);
+    }
+  };
+
   return (
-    <ConnectedAccountContext.Provider value={{ connectedAccount, setConnectedAccount }}>
+    <ConnectedAccountContext.Provider value={{ connectedAccount, connectUiWallet }}>
       {children}
     </ConnectedAccountContext.Provider>
   );
