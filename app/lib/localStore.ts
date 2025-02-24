a7ad44887e1e010123b9b38ee8871068a5af1ac3
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-02-24 13:17:09 -0500

    Use json in local storage instead of csv

--- app/lib/localStore.ts
+++ app/lib/localStore.ts
@@ -6,32 +6,56 @@ import type { IdentifierString } from "@wallet-standard/core";
 const isIdentifierString = (value: string): value is IdentifierString => value.includes(':');
 
 const STORAGE_KEY = 'ripe:connected-wallet';
+interface StoredWalletKeyObj {
+  walletName: string;
+  mainChain: IdentifierString;
+}
+function isStoredWalletKeyObj(value: unknown): value is StoredWalletKeyObj {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    'walletName' in value &&
+    'mainChain' in value &&
+    typeof value.mainChain === 'string' &&
+    isIdentifierString(value.mainChain)
+  );
+}
 
 const saveWallet = (wallet: UiWallet) => {
   // Wallet name doesn't uniquely identify a wallet.
   // E.g. "Phantom" injects multiple wallets (Solana, Bitcoin, Sui, etc.)
   // Each injected wallet has multiple chains, but they're basically mainnet and various testnets.
   // AFAIK, the first chain is always the mainnet, so this key should be unique enough.
-  const storedWalletKey = `${wallet.chains[0]}@${wallet.name}`;
-  localStorage.setItem(STORAGE_KEY, storedWalletKey);
+  const walletName = wallet.name;
+  const mainChain = wallet.chains[0];
+  if (!isIdentifierString(mainChain)) {
+    return console.error('Invalid wallet chain:', mainChain);
+  }
+
+  const storedWalletKeyObj = { walletName, mainChain };
+  if (!isStoredWalletKeyObj(storedWalletKeyObj)) {
+    return console.error('Invalid wallet key object:', storedWalletKeyObj);
+  }
+
+  const storedWalletKeyJson = JSON.stringify({ walletName, mainChain });
+  localStorage.setItem(STORAGE_KEY, storedWalletKeyJson);
 };
 
 const loadWallet = (availableWallets: readonly UiWallet[]) => {
   const storedWalletKey = localStorage.getItem(STORAGE_KEY);
   if (!storedWalletKey) return;
 
-  const [chain, walletName] = storedWalletKey.split('@');
-  if (!chain || !walletName || !isIdentifierString(chain)) {
-    return console.error('Invalid wallet key format:', storedWalletKey);
-  }
+  const storedWalletKeyObj = JSON.parse(storedWalletKey);
+  if (!isStoredWalletKeyObj(storedWalletKeyObj)) return;
+  const { walletName, mainChain } = storedWalletKeyObj;
 
   return availableWallets.find((w: UiWallet) =>
-    w.name === walletName && w.chains.includes(chain)
+    w.name === walletName && w.chains.includes(mainChain)
   );
 };
 
 const removeWallet = () => {
   localStorage.removeItem(STORAGE_KEY);
-}
+};
 
 export { saveWallet, loadWallet, removeWallet };
