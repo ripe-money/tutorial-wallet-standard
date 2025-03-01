a0f1f76dffb603ef122b1f30ee592eb250ae40bf
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-03-01 17:24:31 -0500

    Pass selected wallet in context

--- app/components/SendSplButton.tsx
+++ app/components/SendSplButton.tsx
@@ -1,4 +1,6 @@
-import type { UiWallet } from "@wallet-standard/react";
+import { useContext, useEffect } from "react";
+
+import SelectedWalletContext from "../context/SelectedWalletContext";
 
 import type { SolanaSignAndSendTransactionFeature } from '@solana/wallet-standard';
 import { SolanaSignAndSendTransaction } from '@solana/wallet-standard';
@@ -9,30 +11,45 @@ type SolanaSignAndSendTransactionFeatureType = SolanaSignAndSendTransactionFeatu
 
 import solana from '../lib/solana';
 
-const sendSolUsdcFrom = async (wallet: UiWallet) => {
-  console.log('latest block hash:', await solana.getLatestBlockhash());
-  console.log('Sending 0.01 USDC from', wallet.features);
-  const feature = getWalletFeature(wallet, SolanaSignAndSendTransaction) as SolanaSignAndSendTransactionFeatureType;
-  console.log('Feature:', feature.signAndSendTransaction);
-  // feature.signAndSendTransaction({
-  //   chain: SOLANA_MAINNET_CHAIN,
-  //   account: address('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'),
-  //   transaction: null,
-  // })
-  // const instr = getTransferInstruction({
-  //   source: address('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'),
-  //   destination: address('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'),
-  //   amount: 1,
-  //   authority: address('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'),
-  // });
-  // console.log('Instruction:', instr, typeof instr);
-};
+// const sendSolUsdcFrom = async (wallet: UiWallet) => {
+//   console.log('latest block hash:', await solana.getLatestBlockhash());
+//   console.log('Sending 0.01 USDC from', wallet.features);
+//   const feature = getWalletFeature(wallet, SolanaSignAndSendTransaction) as SolanaSignAndSendTransactionFeatureType;
+//   console.log('Feature:', feature.signAndSendTransaction);
+//   // feature.signAndSendTransaction({
+//   //   chain: SOLANA_MAINNET_CHAIN,
+//   //   account: address('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'),
+//   //   transaction: null,
+//   // })
+//   // const instr = getTransferInstruction({
+//   //   source: address('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'),
+//   //   destination: address('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'),
+//   //   amount: 1,
+//   //   authority: address('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'),
+//   // });
+//   // console.log('Instruction:', instr, typeof instr);
+// };
 
-export const SendSplButton = ({ wallet }: { wallet?: UiWallet }) => {
+export const SendSplButton = () => {
+  const { selectedWallet } = useContext(SelectedWalletContext); // getWalletAccount
+
+  useEffect(() => {
+    const init = async () => {
+      console.log('latest block hash:', await solana.getLatestBlockhash());
+
+      if (!selectedWallet) return;
+      console.log('Wallet:', selectedWallet);
+
+      const feature = getWalletFeature(selectedWallet, SolanaSignAndSendTransaction) as SolanaSignAndSendTransactionFeatureType;
+      console.log('Feature:', feature);
+    };
+    init();
+  }, [selectedWallet]);
+
   return (
     <button
       className="btn btn-primary"
-      onClick={() => console.log(wallet)}
+      onClick={() => console.log('Send USDC')}
     >
       Send 0.01 USDC
     </button>
