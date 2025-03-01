6fb5ebcd2f7af351897bc3e7f47150a0b83b92d2
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-02-28 23:29:37 -0500

    Rewrite getting SOL balance from wallet account (instead of wallet)

--- app/lib/solana.ts
+++ app/lib/solana.ts
@@ -7,19 +7,16 @@ import { address, createSolanaRpc } from '@solana/kit';
 import { SOLANA_MAINNET_CHAIN } from '@solana/wallet-standard';
 export const isSolanaWallet = (wallet: UiWallet) => wallet.chains.includes(SOLANA_MAINNET_CHAIN);
 
-console.log(process.env.NEXT_PUBLIC_SOLANA_RPC)
 const rpc: Rpc<GetBalanceApi & GetTokenAccountsByOwnerApi & GetLatestBlockhashApi> =
   createSolanaRpc(process.env.NEXT_PUBLIC_SOLANA_RPC!);
 
-export const getLatestBlockhash = async () => {
+const getLatestBlockhash = async () => {
   const { value: blockhash } = await rpc.getLatestBlockhash().send();
   return blockhash;
 };
 
 // Get the USDC balance of a Solana wallet account
-export const getSolUsdcBalance = async (account: WalletAccount) => {
-  console.log('Getting USDC balance for', account);
-
+const getUsdcBalance = async (account: WalletAccount) => {
   const { value } = await rpc.getTokenAccountsByOwner(
     address(account.address),
     { mint: address(process.env.NEXT_PUBLIC_SOLANA_USDC_MINT!) },
@@ -29,22 +26,20 @@ export const getSolUsdcBalance = async (account: WalletAccount) => {
   return value[0]?.account.data.parsed.info.tokenAmount.uiAmount || 0;
 };
 
-// // Get the SOL balance of a Solana wallet
-// // Not needed for our app, but keeping it here to test it occasionally.
-// const getSolBalance = async (wallet: UiWallet) => {
-//   console.log('Getting SOL balance for', wallet);
+// Get the SOL balance of a Solana wallet
+// Not needed for our app, but keeping it here to test it occasionally.
+const getSolBalance = async (account: WalletAccount) => {
+  const { value: lamports } =
+    await rpc.getBalance(address(account.address), { commitment: 'confirmed' }).send();
 
-//   const walletAddress = await getWalletAddress(wallet);
-//   if (!walletAddress) return;
+  const formattedValue = new Intl.NumberFormat(undefined, { maximumFractionDigits: 5 }).format(
+    // @ts-expect-error This format string is 100% allowed now.
+    `${lamports}E-9`,
+  );
+  console.log('SOL Balance:', formattedValue, 'SOL');
 
-//   const { value: lamports } =
-//     await rpc.getBalance(address(walletAddress), { commitment: 'confirmed' }).send();
+  return lamports;
+};
 
-//   const formattedValue = new Intl.NumberFormat(undefined, { maximumFractionDigits: 5 }).format(
-//     // @ts-expect-error This format string is 100% allowed now.
-//     `${lamports}E-9`,
-//   );
-//   console.log('Balance:', formattedValue, 'SOL');
-
-//   return lamports;
-// };
+const solana = { getLatestBlockhash, getUsdcBalance, getSolBalance };
+export default solana;
