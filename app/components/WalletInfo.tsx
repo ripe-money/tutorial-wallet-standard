ae61090ec03c2f69516beafa68660310f3a374a8
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-03-02 16:10:17 -0500

    Pass selected wallet into Send SPL button

--- app/components/WalletInfo.tsx
+++ app/components/WalletInfo.tsx
@@ -7,7 +7,7 @@ import WalletBalance from './WalletBalance';
 import SendSplButton from './SendSplButton';
 
 const WalletInfo = () => {
-  const { getWalletAccount } = useContext(SelectedWalletContext);
+  const { selectedWallet, getWalletAccount } = useContext(SelectedWalletContext);
   const [account, setAccount] = useState<WalletAccount | undefined>(undefined);
 
   useEffect(() => {
@@ -23,12 +23,12 @@ const WalletInfo = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold">
-        {!account
-          ? 'No wallet connected'
-          : <>
+        {account && selectedWallet
+          ? <>
               <WalletBalance account={account} />
-              <SendSplButton />
+              <SendSplButton wallet={selectedWallet} />
             </>
+          : 'No wallet connected'
         }
       </h1>
     </div>
