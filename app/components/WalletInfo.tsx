16db3c0d71f3b8a2722c48ba17f2599f2372308e
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-03-01 23:44:46 -0500

    Factor out separate component to show wallet balance

--- app/components/WalletInfo.tsx
+++ app/components/WalletInfo.tsx
@@ -3,15 +3,11 @@ import { useContext, useEffect, useState } from 'react';
 import type { WalletAccount } from '@wallet-standard/core';
 
 import SelectedWalletContext from '../context/SelectedWalletContext';
-import solana from '../lib/solana';
-
-import { SendSplButton } from './SendSplButton';
+import WalletBalance from './WalletBalance';
 
 const WalletInfo = () => {
   const { getWalletAccount } = useContext(SelectedWalletContext);
   const [account, setAccount] = useState<WalletAccount | undefined>(undefined);
-
-  const [balance, setBalance] = useState<number | null>(null);
 
   useEffect(() => {
     const updateBalance = async () => {
@@ -19,8 +15,6 @@ const WalletInfo = () => {
       if (!account) return;
 
       setAccount(account);
-      solana.getSolBalance(account);
-      solana.getUsdcBalance(account).then(balance => setBalance(balance));
     };
     updateBalance();
   }, [getWalletAccount]);
@@ -30,24 +24,11 @@ const WalletInfo = () => {
       <h1 className="text-3xl font-bold">
         {!account
           ? 'No wallet connected'
-          : balance === null
-            ? 'Loading balance...'
-            : (<>
-                {formatAddress(account.address)} has {formatBalance(balance)} USDC
-                <SendSplButton />
-              </>)
+          : <WalletBalance account={account} />
         }
       </h1>
     </div>
   );
 };
-
-const formatAddress = (address: string) => {
-  return `${address.slice(0, 4)}...${address.slice(-4)}`;
-}
-
-const formatBalance = (balance: number) => {
-  return new Intl.NumberFormat(undefined, { maximumFractionDigits: 4 }).format(balance);
-}
 
 export default WalletInfo;
