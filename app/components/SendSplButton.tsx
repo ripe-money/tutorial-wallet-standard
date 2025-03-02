e96e3bc0735d4cbdf17eb9d454a9ab2d2d7d9d2f
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-03-02 18:41:05 -0500

    Use `useConnect` from wallet standard within SendSplButton

--- app/components/SendSplButton.tsx
+++ app/components/SendSplButton.tsx
@@ -1,53 +1,65 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import { getWalletFeature, type UiWallet } from '@wallet-standard/react';
+// import { getWalletFeature } from '@wallet-standard/react';
+import { useConnect, type UiWalletAccount, type UiWallet } from '@wallet-standard/react';
 
-import type { SolanaSignAndSendTransactionFeature } from '@solana/wallet-standard';
-import { SolanaSignAndSendTransaction } from '@solana/wallet-standard';
-type SolanaSignAndSendTransactionFeatureType = SolanaSignAndSendTransactionFeature[typeof SolanaSignAndSendTransaction];
+// import type { SolanaSignAndSendTransactionFeature } from '@solana/wallet-standard';
+// import { SolanaSignAndSendTransaction } from '@solana/wallet-standard';
+// type SolanaSignAndSendTransactionFeatureType = SolanaSignAndSendTransactionFeature[typeof SolanaSignAndSendTransaction];
 
 // import { getTransferInstruction } from '@solana-program/token';
 
-import solana from '../lib/solana';
+// import solana from '../lib/solana';
 
-import {
-  address,
-  // appendTransactionMessageInstruction,
-  createTransactionMessage,
-  pipe,
-  setTransactionMessageFeePayer,
-  setTransactionMessageLifetimeUsingBlockhash,
-} from '@solana/kit';
+// import {
+//   address,
+//   // appendTransactionMessageInstruction,
+//   createTransactionMessage,
+//   pipe,
+//   setTransactionMessageFeePayer,
+//   setTransactionMessageLifetimeUsingBlockhash,
+// } from '@solana/kit';
 
 const SendSplButton = ({ wallet }: { wallet: UiWallet }) => {
-  console.log('Wallet:', wallet);
+  const [isConnecting, connect] = useConnect(wallet);
+  const [connectedAccounts, setConnectedAccounts] = useState<readonly UiWalletAccount[]>();
 
   useEffect(() => {
     const init = async () => {
-      const latestBlockhash = await solana.getLatestBlockhash();
-      console.log('latest block hash:', latestBlockhash);
+      // const latestBlockhash = await solana.getLatestBlockhash();
+      // console.log('latest block hash:', latestBlockhash);
 
-      const feature = getWalletFeature(wallet, SolanaSignAndSendTransaction) as SolanaSignAndSendTransactionFeatureType;
-      console.log('Feature:', feature);
+      // const feature = getWalletFeature(wallet, SolanaSignAndSendTransaction) as SolanaSignAndSendTransactionFeatureType;
+      // console.log('Feature:', feature);
 
-      const feePayer = address('AxZfZWeqztBCL37Mkjkd4b8Hf6J13WCcfozrBY6vZzv3');
-      const message = pipe(
-        createTransactionMessage({ version: 'legacy' }),
-        m => setTransactionMessageFeePayer(feePayer, m),
-        m => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, m),
-        // m => appendTransactionMessageInstruction(getAddMemoInstruction({ memo: text }), m),
-      );
-      console.log('Message:', message);
+      // const feePayer = address('AxZfZWeqztBCL37Mkjkd4b8Hf6J13WCcfozrBY6vZzv3');
+      // const message = pipe(
+      //   createTransactionMessage({ version: 'legacy' }),
+      //   m => setTransactionMessageFeePayer(feePayer, m),
+      //   m => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, m),
+      //   // m => appendTransactionMessageInstruction(getAddMemoInstruction({ memo: text }), m),
+      // );
+      // console.log('Message:', message);
+
+      if (connectedAccounts) return;
+      connect().then((accounts) => {
+        setConnectedAccounts(accounts);
+        console.log('Connected accounts:', accounts);
+        if (accounts.length === 0) return;
+        const account = accounts[0];
+        console.log('Account:', account);
+      });
     };
     init();
-  }, [wallet]);
+  }, [connect, connectedAccounts, wallet]);
 
   return (
     <button
       className="btn btn-primary"
-      onClick={() => console.log('Send USDC')}
+      onClick={() =>console.log('Button clicked')}
     >
-      Send 0.01 USDC
+      Send 0.01 USDC&nbsp;
+      {isConnecting && 'Connecting...'}
     </button>
   );
 };
