f75f4731090121eefcf24c53e26cdef2d5b8317b
Chuck Lam <939400+chucklam@users.noreply.github.com>
2025-03-03 15:56:07 -0500

    Send out memo transaction

--- app/components/SendSplButton.tsx
+++ app/components/SendSplButton.tsx
@@ -1,53 +1,59 @@
-import { useEffect } from "react";
+import type { UiWalletAccount } from '@wallet-standard/react';
 
 import { getWalletAccountFeature } from '@wallet-standard/react';
-import { type UiWalletAccount } from '@wallet-standard/react';
-
 import type { SolanaSignAndSendTransactionFeature } from '@solana/wallet-standard';
 import { SolanaSignAndSendTransaction } from '@solana/wallet-standard';
 type SolanaSignAndSendTransactionFeatureType = SolanaSignAndSendTransactionFeature[typeof SolanaSignAndSendTransaction];
 
+import { useWalletAccountTransactionSendingSigner } from '@solana/react';
+import { getAddMemoInstruction } from '@solana-program/memo';
 // import { getTransferInstruction } from '@solana-program/token';
 
 import solana from '../lib/solana';
 
-// import {
-//   address,
-//   // appendTransactionMessageInstruction,
-//   createTransactionMessage,
-//   pipe,
-//   setTransactionMessageFeePayer,
-//   setTransactionMessageLifetimeUsingBlockhash,
-// } from '@solana/kit';
+import {
+  appendTransactionMessageInstructions,
+  createTransactionMessage,
+  getBase58Decoder,
+  pipe,
+  setTransactionMessageFeePayerSigner,
+  setTransactionMessageLifetimeUsingBlockhash,
+  signAndSendTransactionMessageWithSigners,
+} from '@solana/kit';
 
+// https://github.com/anza-xyz/kit/tree/main/packages/react
 const SendSplButton = ({ account }: { account: UiWalletAccount }) => {
+  const signer = useWalletAccountTransactionSendingSigner(account, 'solana:devnet');
 
-  useEffect(() => {
-    const init = async () => {
-      const latestBlockhash = await solana.getLatestBlockhash();
-      console.log('latest block hash:', latestBlockhash);
+  const onClick = async () => {
+    const latestBlockhash = await solana.getLatestBlockhash();
+    console.log('latest block hash:', latestBlockhash);
 
-      const feature = getWalletAccountFeature(account, SolanaSignAndSendTransaction) as SolanaSignAndSendTransactionFeatureType;
-      console.log('Feature:', feature);
+    const feature = getWalletAccountFeature(account, SolanaSignAndSendTransaction) as SolanaSignAndSendTransactionFeatureType;
+    console.log('Feature:', feature);
 
-      // const feePayer = address('AxZfZWeqztBCL37Mkjkd4b8Hf6J13WCcfozrBY6vZzv3');
-      // const message = pipe(
-      //   createTransactionMessage({ version: 'legacy' }),
-      //   m => setTransactionMessageFeePayer(feePayer, m),
-      //   m => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, m),
-      //   // m => appendTransactionMessageInstruction(getAddMemoInstruction({ memo: text }), m),
-      // );
-      // console.log('Message:', message);
-    };
-    init();
-  }, [account]);
+    const message = pipe(
+      createTransactionMessage({ version: 'legacy' }),
+      m => setTransactionMessageFeePayerSigner(signer, m),
+      m => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, m),
+      m => appendTransactionMessageInstructions([
+        getAddMemoInstruction({ memo: 'Your money is ripe' }),
+      ], m),
+    );
+    console.log('Transaction Message:', message);
+
+    const signatureBytes = await signAndSendTransactionMessageWithSigners(message);
+
+    const base58Signature = getBase58Decoder().decode(signatureBytes);
+    console.log(`View transaction: https://explorer.solana.com/tx/${base58Signature}?cluster=devnet`);
+  };
 
   return (
     <button
       className="btn btn-primary"
-      onClick={() =>console.log('Button clicked')}
+      onClick={onClick}
     >
-      Send 0.01 USDC
+      Send memo onchain
     </button>
   );
 };
